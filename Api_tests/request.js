7e764f6961ebc5ba00cd144c7ce42faa567a6a88
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-02-09 11:25:37 +0100

    Update request.js

--- Api_tests/request.js
+++ Api_tests/request.js
@@ -4,7 +4,16 @@ const sol_price = "https://frontend-api-v3.pump.fun/sol-price"
 const eras = "https://frontend-api-v3.pump.fun/eras" 
 
 
-
+//get responses
+function fetch_ER () {
+    fetch(eras)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log("era", data);
+    });
+}
 function fetch_MP () {
     fetch(my_profile_url)
     .then(response => {
@@ -12,16 +21,6 @@ function fetch_MP () {
     })
     .then(data => {
         console.log("my_profile_url", data);
-    });
-}
-
-function fetch_LU () {
-    fetch(login_url)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log("login_url", data);
     });
 }
 
@@ -35,18 +34,38 @@ function fetch_SP () {
     });
 }
 
-function fetch_ER () {
-    fetch(eras)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log("era", data);
-    });
+
+//datacollection_process
+const nacl = require('tweetnacl');
+const privateKey = Uint8Array.from([...]);
+const walletAddress = "BL4NTkMsGB1pf2ju43qQDYhPtpKSSrFUaX4kU5ZoqPE";
+const timestamp = Date.now();
+const message = `Sign this message to authenticate: ${timestamp}`;
+const messageBytes = Buffer.from(message);
+const signature = nacl.sign.detached(messageBytes, privateKey);
+
+
+//data input
+const data = {
+    address: walletAddress,
+    signature: Buffer.from(signature).toString('hex'),
+    timestamp: timestamp
 }
 
 
+//option requests
+const requestOptions = {
+    method: "POST",
+    header: {
+        "Content-type": "application/json",
+    },
+    body: JSON.stringify(data)
+}
+
+
+
+fetch_ER();
 fetch_MP();
-fetch_ER();
-fetch_LU();
-fetch_ER();
+fetch_SP();
+console.log(Date.now())
+

8880eedfc3344144838e149effeb23cf8cb0a231
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-02-14 12:12:14 +0100

    Update commenter.js

--- commenter.js
+++ commenter.js
@@ -10,20 +10,26 @@ async function main() {
         const { AuthToken, CommentToken } = await x_aws_proxy_token;
         console.log("AuthToken:", AuthToken);
         console.log("Generated Token:", CommentToken);
-         await delay(3412);
-
+        await delay(6412);
         const response = await fetch(comment_url, {
             method: "POST",
             body: JSON.stringify({
-                mint: "GYfrXKLRKhzeQvWygq7EygBRoDwsH5GGkhD5RVUZpump",
-                text: "test"
+                mint: "CUSbQC3LLB5w6ZjZSAaY9mTnc6kR4igufVMsN9tBQyvm",
+                text: "Thommi3 & tr4m0ryp zijn de bot aan het testen; als je dit ziet werkt de bot:))))"
             }),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${AuthToken}`,
                 'X-Aws-Proxy-Token': CommentToken,
-                'Accept': 'application/json',
-                'Cookie': `auth_token=${AuthToken}` 
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+                'Origin': 'https://pump.fun',
+                'Referer': 'https://pump.fun/',
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-site',
+                'Cookie': `auth_token=${AuthToken}`
             }
         });
 
